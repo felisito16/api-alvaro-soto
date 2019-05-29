be2caed666f61a4f4357a76c5c1b4394fa817566
Felix <felix.delrio.1@gmail.com>
2019-05-29 14:37:20 +0200

    mAS COSAS

--- API soto-alvaro/src/routes/employees.js
+++ API soto-alvaro/src/routes/employees.js
@@ -5,12 +5,37 @@ const mysqlCOnnection = require("../database")
 
 router.get("/", (req, res) => {
     mysqlCOnnection.query("SELECT * FROM employees", (err, rows, fields) => {
-        if(!err) {
+        if (!err) {
             res.json(rows)
         } else {
             console.log(err);
         }
     })
+})
+
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
+    console.log(id);
+    mysqlCOnnection.query("SELECT * FROM employees"
+        + " WHERE id = ?", [id], (err, rows, fields) => {
+            if (!err) {
+                res.json(rows[0])
+            } else {
+                console.log(err);
+            }
+        })
+})
+
+router.post("/ ", (req, res) => {
+    const { id, name, salary } = req.body;
+    mysqlCOnnection.query("SELECT * FROM employees"
+        + " WHERE id = ?", [id], (err, rows, fields) => {
+            if (!err) {
+                res.json(rows)
+            } else {
+                console.log(err);
+            }
+        })
 })
 
 module.exports = router
